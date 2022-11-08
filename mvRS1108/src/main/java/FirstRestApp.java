e40b254c63522a2b1c226911a5d40a35890286b9
student <student@DESKTOP-4ARRLB2>
2022-11-08 15:11:42 +0800

    Second Code

--- mvRS1108/src/main/java/FirstRestApp.java
+++ mvRS1108/src/main/java/FirstRestApp.java
@@ -11,6 +11,6 @@ public class FirstRestApp extends Application
 {
     @Override
     public Set<Class<?>> getClasses() {
-        return new HashSet(Arrays.asList(CustomerRestService.class));
+        return new HashSet(Arrays.asList(CustomerRestService.class,StudentRestService.class));
     }
 }
